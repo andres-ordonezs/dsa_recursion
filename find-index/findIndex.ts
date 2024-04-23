a73f3e1007765bda3742f30e06e9ecdc8352d82d
Yagiz Ulasoglu <yulasoglu@gmail.com>
2024-04-23 14:30:28 -0700

    find index

--- find-index/findIndex.ts
+++ find-index/findIndex.ts
@@ -1,7 +1,23 @@
 /** findIndex: return index of val in arr (or -1 if val is not present). */
 
 function findIndex(arr: number[], val: number): number {
-  return 42;
+  if (arr.length === 0) {
+    return -1;
+  }
+  if (arr[0] === val) {
+    return 0;
+  }
+  const index = findIndex(arr.slice(1), val);
+
+  return index === -1 ? -1 : (index + 1);
 }
 
+
+// [1, 2, 3], 3 =>2
+
+//[2, 3], 3 => 1
+
+// [3], 3 => 0
+
+//[2, 3], 3     =>
 export { findIndex };
