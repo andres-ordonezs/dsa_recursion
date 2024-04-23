9aa9f79a0fee0192cc6aa4b54d6a1dc59d9468cf
Yagiz Ulasoglu <yulasoglu@gmail.com>
2024-04-23 13:42:12 -0700

    find

--- find/find.ts
+++ find/find.ts
@@ -1,7 +1,19 @@
 /** find: return t/f is val is in arr. */
 
 function find(arr: number[], val: number): boolean {
-  return true;
+  // return true;
+  if (arr.length === 0) {
+    return false;
+  }
+  return arr[0] === val || find(arr.slice(1), val);
 }
 
 export { find };
+
+
+/*
+[1, 2, 3], 3
+1 === 3 || find([2,3], 3)
+2 === 3 || find([3], 3)
+3 === 3 || find([], 3)
+*/
