0cf7077dac9c3e25571f4c4298f16e3d74741872
andres-ordonezs <andresmw.89@gmail.com>
2024-04-23 17:47:58 -0400

    Binary Search

--- binary-search/binarySearch.ts
+++ binary-search/binarySearch.ts
@@ -1,8 +1,51 @@
 /** binarySearch: given a sorted array of numbers, and a value,
  * return true if val is in array, false if not present). */
 
-function binarySearch(arr: number[], val: number, left = 0, right = arr.length): boolean {
-  return false;
+function binarySearch(
+  arr: number[],
+  val: number,
+  left = 0,
+  right = arr.length
+): boolean {
+  if (left > right) {
+    return false;
+  }
+
+  const midPoint = Math.floor((left + right) / 2);
+
+  if (arr[midPoint] === val) {
+    return true;
+  }
+
+  if (arr[midPoint] > val) {
+    right = midPoint - 1;
+    return binarySearch(arr, val, left, right);
+  }
+
+  left = midPoint + 1;
+  return binarySearch(arr, val, left, right);
+
+  // if (arr[midPoint] < val) {
+  //   left = midPoint + 1;
+  //   return binarySearch(arr, val, left, right);
+  // }
 }
 
-export { binarySearch };
+export {binarySearch};
+
+/*
+[1,2,3,4], 4
+
+binarySearch([1,2,3,4], 3,left=0, right=4)
+-> midPoint = Math.floor(0+4 / 2) = 2 =idx
+-> arr[idx=2] < 4
+-> left: arr[idx=2 + 1]
+binarySearch()
+
+base case: if(left>right){
+  return false
+}
+
+
+
+*/
