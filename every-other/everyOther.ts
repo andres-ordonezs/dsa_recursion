f31324f054ea03ef4b2f897598f4af57bddd42a4
Yagiz Ulasoglu <yulasoglu@gmail.com>
2024-04-23 12:15:03 -0700

    every-other is done

--- every-other/everyOther.ts
+++ every-other/everyOther.ts
@@ -1,7 +1,13 @@
 /** everyOther: return a string with every other letter. */
 
 function everyOther(str: string): string {
-  return "x";
+  // return "x";
+  if (str.length <= 1) {
+    return str;
+  }
+
+  return str[0] + everyOther(str.slice(2));
 }
 
 export { everyOther };
+
